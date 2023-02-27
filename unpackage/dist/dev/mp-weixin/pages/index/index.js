41da39dcc6074c2e6bb55650e0943ee4836339ee
bp-liangjh <liangjiahao@beihui.com>
2023-02-27 17:41:09 +0800

    代码提交

--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -15,5 +15,5 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
     a: common_vendor.t($data.title)
   };
 }
-var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/web/github/kyle/uniapp_basic/pages/index/index.vue"]]);
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/web/github/kyle/my_miniProgram/pages/index/index.vue"]]);
 wx.createPage(MiniProgramPage);
